6f6b57ac252ae6ea76ec4e725591ecae0e71e5a3
Renato Barros <rennatobarros@gmail.com>
2017-04-08 21:21:52 -0300

    Add the function 'pttomorse'

--- src/libs/functions.cpp
+++ src/libs/functions.cpp
@@ -1,8 +1,25 @@
 #include "global.hpp"
 
-//string pttomorse(){
+void pttomorse(string pttext, string morse[], char alphabet[]){
+    int length = pttext.length();
+    string temp = " "; // String temporária para amarzenar o código traduzido.
+    
+    // Converte toda a string em letras minusculas
+    transform(pttext.begin(), pttext.end(), pttext.begin(), ::toupper);
 
-//}
+    
+    for(i = 0; i < length; i++){
+        for(j = 0; j < 36; j++){
+            if(pttext[i] == alphabet[j]){
+                //cout << morse[j] << " ";
+                temp += morse[j] + " ";
+                break;
+            }
+        }
+    }
+    cout << "TEXT: " << pttext << endl;
+    cout << "MORSE CODE: " << temp << endl;
+}
 
 //string morsetopt(){
 
